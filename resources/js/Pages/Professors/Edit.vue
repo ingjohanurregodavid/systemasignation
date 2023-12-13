7865e59c28de48f526bf7eb9137a7a8a5d4f9d52
ingjohanurregodavid <114048400+ingjohanurregodavid@users.noreply.github.com>
2023-12-13 04:12:03 -0500

    Ajuste de nombre para edición de profesor

--- resources/js/Pages/Professors/Edit.vue
+++ resources/js/Pages/Professors/Edit.vue
@@ -18,11 +18,11 @@ const form=useForm({
 </script>
 
 <template>
-    <Head title="Create Professors" />
+    <Head title="Edit Professors" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Create Professors</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Edit Professors</h2>
         </template>
 
         <div class="py-12">
